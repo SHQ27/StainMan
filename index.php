dcd95b1d85a39facafe2b6763ec6bd556ae01d0f
Sebastián Hernández <Sebi@Leonardo.local>
2014-12-14 22:10:20 -0300

    Partials incluidos

--- index.php
+++ index.php
@@ -13,25 +13,20 @@
 
 		<!-- HEADER -->
 
-		<div id="header">
-			<div class="top-banner">
-				<img class="header-logo" src="img/mainLogo.png">
-				<img class="img-cut img-responsive" src="img/cut.png">
-			</div>
-		</div>	
-		<div class="nav-bar">
-			<ul class="nav-bar-menu">
-				<li><a class="nav-bar-items" href="#!">EL MUSICAL</a></li>
-				<li><a class="nav-bar-items" href="#!">AUTORES</a></li>
-				<li><a class="nav-bar-items" href="#!">ELENCO</a></li>
-				<li><a class="nav-bar-items" href="#!">FICHA TÉCNICA</a></li>
-				<li><a class="nav-bar-items" href="#!">PROTAGONISTAS</a></li>
-				<li class="buy-border"><a class="nav-bar-items nav-bar-buy" href="#!">COMPRAR TICKETS</a></li>
-			</ul>
-		</div>
+		<?php include('includes/header.php') ?>
+
+		<!-- CONTENT -->
+
+		<?php include('includes/inicio.php') ?>
+		<?php include('includes/autores.php') ?>
+		<?php include('includes/elenco.php') ?>
+		<?php include('includes/ficha.php') ?>
+		<?php include('includes/protagonistas.php') ?>
 
 		<!-- FOOTER -->
+
 		<?php include('includes/footer.php') ?>
+		
 	</div>
 </body>
 </html>
