0e3f4b7f8f94a3b7dab499eed21b3b29a9e8340d
Emanuel Mahler <EmanuelMahler@Emanuels-MacBook-Pro.local>
2014-12-15 11:13:54 -0300

    autores terminado

--- includes/autores.php
+++ includes/autores.php
@@ -1,39 +1,46 @@
-<div id="autores-container">
+<div class="autores-container">
 	<div class="autores-inner-container">
 		<div class="wasser-container">
-			<div class="wasser-img col-lg-3 col-md-3 col-sm-3 col-xs-3">
+			<div class="wasser-img">
 				<img class="img-responsive" src="../img/DaleWasserman.jpg">
 			</div>
-			<div class="wasser-p">
+			<div class="wasser-text">
 				<h2>DALE WASSERMAN</h2>
-				<p>Dale Wasserman wrote for theater, television and film for more than 50 years and is best known for the musical Man of La Mancha, a multiple Tony Award winner. He also wrote the stage play One Flew Over the Cuckoo’s Nest, based on Ken Kesey’s novel, which has won several Tony Awards. Both shows continue to be produced nationally and internationally with an estimated 300 productions a year.
-					In January 1979, Dale met Martha Nelly Garza, who became his wife, loyal partner and loving companion, as he quoted in his book, The Impossible Musical(2003). More than once, Dale commented that Martha Nelly was the best thing that ever happened to him (aside from MOLM) and that it was their 30-year partnership that had been the greatest contribution to extending his life and his talents to age 94. Together, they worked on numerous musicals and several new plays which can be found on this website under RECENT & READY WORKS.
-					On December 21, 2008, Dale Wasserman, with his loving wife at his side, passed away peacefully of natural causes at his home in Paradise Valley, Arizona. Martha N. Wasserman is now the sole Owner/Licensor of Dale’s intellectual properties.</p>
-			</div>
-		</div>
-		<div class="three-author-container">
-			<div class="three-authors col-lg-6 col-md-6 col-sm-6">
-				<div>
-					<h2>MITCH LEIGH</h2>
-				</div>
-				<div class="three-authors-p">
-					<p>MITCH LEIGH (Music) (January 30, 1928 - March 16, 2014) composed Man of La Mancha, which originally opened on Broadway in 1965 and went on to win five Tony Awards including Best Musical. Man of La Mancha ran for 2328 performances on Broadway, making it one of the greatest musicals of all time, spawning numerous national and international productions.  His other Broadway scores include Cry for Us All, Sarava, Chu Chem, and Ain’t Broadway Grand.  He is the recipient of numerous awards including the NY Drama Critics Circle Award and the Contemporary Classics Awards from the Songwriters’ Hall of Fame for “The Impossible Dream,” and he is the first composer to receive the Yale Arts Award for Outstanding Achievement in Musical Composition.  He has also been honored as the only living composer whose work was included in the Metropolitan Opera’s Centennial Celebration.  Mr. Leigh produced and directed Yul Brynner’s farewell tour of The King and I.  In 2001 the Music School at Yale University was named Leigh Hall.</p>
-				</div>
-			</div>
-			<div class="three-authors col-lg-6 col-md-6 col-sm-6">
-				<div>
-					<h2>JOE DARION</h2>
-				</div>
-				<div class="three-authors-p">
-					<p>JOE DARION (Lyrics) (January 30, 1917 - June 6, 2001) has worked in every field in which words are put to music, from popular songs to works for the concert stage.  His opera based on Don Marquis’ immortal characters Archy and Mehitabel, was turned into the Broadway musical Shinbone Alley, for which Mr. Darion supplied book and lyrics.  Popular songs for which he has supplied the lyrics include “Ricochet,” “Changing Partners,” and “Midnight Rain,” selling in the tens of millions.  One of the most popular has been “The Impossible Dream,” from Man of La Mancha, which won Darion the Tony Award for Best Lyrics. He also supplied the lyrics for the Broadway musical Illya Darling, adapted from the film Never on Sunday.  In addition to the Tony Award, he has the Outer Critics Circle Award, the Gabriel Award, the Ohio State Award, and the International Broadcasting Award.</p>
-				</div>
-			</div>	
-		</div>
-		<div class="masllorens">
-				<h2>FERNANDO MASLLORENS Y FEDERICO GONZÁLEZ DEL PINO</h2>
 				<p>Dale Wasserman wrote for theater, television and film for more than 50 years and is best known for the musical Man of La Mancha, a multiple Tony Award winner. He also wrote the stage play One Flew Over the Cuckoo’s Nest, based on Ken Kesey’s novel, which has won several Tony Awards. Both shows continue to be produced nationally and internationally with an estimated 300 productions a year.
 				In January 1979, Dale met Martha Nelly Garza, who became his wife, loyal partner and loving companion, as he quoted in his book, The Impossible Musical(2003). More than once, Dale commented that Martha Nelly was the best thing that ever happened to him (aside from MOLM) and that it was their 30-year partnership that had been the greatest contribution to extending his life and his talents to age 94. Together, they worked on numerous musicals and several new plays which can be found on this website under RECENT & READY WORKS.
 				On December 21, 2008, Dale Wasserman, with his loving wife at his side, passed away peacefully of natural causes at his home in Paradise Valley, Arizona. Martha N. Wasserman is now the sole Owner/Licensor of Dale’s intellectual properties.</p>
 			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="mitch-container">
+			<div class="mitch-title">
+				<h2>MITCH LEIGH</h2>
+				<p>(Música Original)</p>
+			</div>
+			<div class="mitch-text">
+				<p>MITCH LEIGH (Music) (January 30, 1928 - March 16, 2014) composed Man of La Mancha, which originally opened on Broadway in 1965 and went on to win five Tony Awards including Best Musical. Man of La Mancha ran for 2328 performances on Broadway, making it one of the greatest musicals of all time, spawning numerous national and international productions.  His other Broadway scores include Cry for Us All, Sarava, Chu Chem, and Ain’t Broadway Grand.  He is the recipient of numerous awards including the NY Drama Critics Circle Award and the Contemporary Classics Awards from the Songwriters’ Hall of Fame for “The Impossible Dream,” and he is the first composer to receive the Yale Arts Award for Outstanding Achievement in Musical Composition.  He has also been honored as the only living composer whose work was included in the Metropolitan Opera’s Centennial Celebration.  Mr. Leigh produced and directed Yul Brynner’s farewell tour of The King and I.  In 2001 the Music School at Yale University was named Leigh Hall.</p>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="darion-container">
+			<div class="darion-title">
+				<h2>JOE DARION</h2>
+				<p>(Letras)</p>
+			</div>
+			<div class="darion-text">
+				<p>JOE DARION (Lyrics) (January 30, 1917 - June 6, 2001) has worked in every field in which words are put to music, from popular songs to works for the concert stage.  His opera based on Don Marquis’ immortal characters Archy and Mehitabel, was turned into the Broadway musical Shinbone Alley, for which Mr. Darion supplied book and lyrics.  Popular songs for which he has supplied the lyrics include “Ricochet,” “Changing Partners,” and “Midnight Rain,” selling in the tens of millions.  One of the most popular has been “The Impossible Dream,” from Man of La Mancha, which won Darion the Tony Award for Best Lyrics. He also supplied the lyrics for the Broadway musical Illya Darling, adapted from the film Never on Sunday.  In addition to the Tony Award, he has the Outer Critics Circle Award, the Gabriel Award, the Ohio State Award, and the International Broadcasting Award.</p>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="masllorens-container">
+			<div class="masllorens-title">
+				<h2>FERNANDO MASLLORENS <br>FEDERICO GONZÁLEZ</h2>
+				<p>(Traducción y Versión)</p>
+			</div>
+			<div class="masllorens-text">
+				<p>Empezaron su actividad teatral en 1978. A partir de entonces se han desempeñado como traductores, adaptadores, productores, empresarios teatrales y agentes literarios. Entre sus primeras traducciones y adaptaciones se encuentran: Caja de Sombras; Papá; Emily; Adiós, Mamá; Chispas; Cartas de Amor; Mamá; Soufflé; Camille; Extraña Pareja Femenina; Charlotte; Flores de Acero; Danza de Verano, etc. En las últimas décadas tradujeron y adaptaron: Amor-Valor-Compasión; Master Class; Humores que Matan; El Cuarto Azul; Sólo Cuando Me Río; Sylvia; Ofensa; Juana de Lorena; Bravo, Caruso; Extraña Pareja; El Asesinato de la Enfermera Jorge; La Noche de la Iguana; Hijos; Angeles en América I y II; Socios en el Amor; Y las Sirenas Cantarán...; Cristales Rotos; La vita y Virginia; Cloer; El Juego del Bebé; Variaciones Enigmáticas; Monólogos de la Vagina; Camino a la Meca; Querido Mentiroso; Copenhague; Panorama desde el Puente; Aplausos; Fiebre de un Sábado a la Noche; Zorba; El Violinista en el Tejado; La Prueba; El Gran Regreso; Shakespeare Comprimido; En Casa/En Kabul; La Brisa de la Vida; La Profesión de la Sr.a Warren; Pequeños Crímenes Conyugales; Cinco Mujeres con el Mismo Vestido; Los Productores; Visitando al Sr. Green; La Duda; ¿Quién le teme a Virginia Woolf?; Yo soy mi Propia Mujer; Conversaciones Después de un Entierro; El Último Yankee; Oscar y la Dama Rosa; Muerte de un Viajante; Tres Versiones de la Vida; El Hombre Inesperado; Codicia; Gorda; Rent; Hairspray; Cómo Aprendí a Manejar; El Diario de Anna Frank; Baraka; Jack se fue a Remar; La Noche que Larry Kramer me Besó; Una Cierta Piedad; La Vuelta al Hogar; El Joven Frankenstein; La Forma de las Cosas; Dos Menos; La Cocina; Raíces; Souvenir; Mi Primera Vez; Chicas del Calendario; Rumores; Un Dios Salvaje; El Descenso del Monte Morgan; Pirañas; Los Reyes de la Risa; Sweeney Todd; Lluvia Constante; Rain Main; Espejos Circulares; mineros; Todos Felices, Buena Gente; El Hijo de P*!@ del Sombrero; La Cabra; Cenizas; Las Brujas de Salem; Algunas Mujeres a las que les c..... la Vida; El León en Invierno; Amadeus; Le Prénom; Escenas de la Vida Conyugal; Vale Todo; 33 Variaciones; El Hombre Elefante; El Placard; Los Elegidos; Red; Venus en Puel, entre otras. Fueron nominados para el Premio Teatro del Mundo (Universidad Nacional de Buenos Aires) en varias oportunidades y ganaron dicho premio en 2002 por su versión de Copenhague. Recibieron el Diploma al Mérito Konex 2004 entre las 100 figuras más destacadas de la década 1994-2003 de las Letras Argentinas.</p>
+			</div>
+			<div class="clear"></div>
 		</div>
 	</div>
+</div>
