24ac1fb986040c7d1acde85bc7493647a1248ddc
Sebastián Hernández <Sebi@Leonardo.local>
2014-12-16 03:23:48 -0300

    Sitio under construction en carpeta construction

---
+++ construction/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>EL HOMBRE DE LA MANCHA</title>
+	<link rel="stylesheet" href="./css/bootstrap.css">
+	<link rel="stylesheet" href="./css/enter.css">
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="col-lg-2 col-md-2 col-sm-2"></div>
+	<div class="enter col-lg-8 col-md-8 col-sm-8 col-xs-12">
+		<div class="enter-in">
+			<img src="./img/logoEnter.png">
+			<div id='premier' class='row text-center'>
+				<h1 class='premier-title'>- PROXIMAMENTE -</h1>
+				<a class="nav-bar-buy-big" target="_blank" href="https://www.plateanet.com/Comprar/el-hombre-de-la-mancha---un-musical">COMPRAR TICKETS</a>
+			</div>
+		</div>
+	</div>
+	<div class="col-lg-2 col-md-2 col-sm-2"></div>
+</body>
+	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/fadeEnter.js"></script>
+</html>
