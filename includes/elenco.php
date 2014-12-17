a6c848c69b4b2e047f9845dcd1d138f626a50ccc
Emanuel Mahler <EmanuelMahler@Emanuels-MacBook-Pro.local>
2014-12-17 18:16:06 -0300

    elenco

---
+++ includes/elenco.php
@@ -0,0 +1,38 @@
+<div class="elenco-container">
+	<div class="elenco-inner-container">
+		<h1>ELENCO</h1>
+		<div class="elenco-titles-container">
+			<div>
+				<h3 class="elenco-titles">PEPE CIBRIÁN</h3>
+				<p class="elenco-subtitles">(EL QUIJOTE)</p>
+			</div>
+			<div class="elenco-raul-cecilia">
+				<div class="raul">
+					<h3 class="elenco-titles elenco-titles-raul">RAÚL LAVIÉ</h3>
+					<p class="elenco-subtitles elenco-subtitles-raul">(SANCHO PANZA)</p>
+				</div>
+				<div class="cecilia">
+					<h3 class="elenco-titles elenco-titles-cecilia">CECILIA MILONE</h3>
+					<p class="elenco-subtitles elenco-subtitles-cecilia">(DULCINEA DEL TOBOSO)</p>
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+		<div class="elenco-acompanados">
+			<h4>ACOMPAÑADOS POR</h4>
+			<ul>
+				<li>Hernán Kuttel</li>
+				<li>Patricio Witis</li>
+				<li>Christian Alladio</li>
+				<li>Gastón Avendaño</li>
+				<li>Lucas Arbúes</li>
+				<li>Alejandro Poggio</li>
+				<li>Bruno Pedicone</li>
+				<li>Martín Selle</li>
+				<li>Ivano Nardacchione</li>
+				<li>Ana Victoria de Vicentiis</li>
+				<li></li>
+			</ul>
+		</div>
+	</div>
+</div>
